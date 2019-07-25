e7c484c9df830db28815ff56cd90e32bc71591be
Elliott Smith <elliottsmith1@hotmail.co.uk>
2019-07-25 16:34:46 +0100

    Added enemy building placement

--- GA RTS/Assets/Scripts/Managers/AIManager.cs
+++ GA RTS/Assets/Scripts/Managers/AIManager.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEditor;
 
 public class AIManager : MonoBehaviour
 {
+    [Header("Building prefabs")]
+    [SerializeField] GameObject barracks;
+    [SerializeField] GameObject archeryRange;
+    [SerializeField] GameObject house;
+    [SerializeField] GameObject lumberMill;
+    [SerializeField] GameObject market;
+
     [SerializeField] GameObject enemyPrefab;
 
-    [SerializeField] float spawnTimer = 0.0f;
-    [SerializeField] float spawnDelay = 100.0f;
     [SerializeField] float groupSize = 5;
-    [SerializeField] float spawnMultiplier = 1.25f;
 
     private Vector3 playerPos = new Vector3(10, 0, 10);
 
     private List<GameObject> enemyBuildings = new List<GameObject>();
+
+    //base building
+    private float expansionFactor = 10.0f;
+    private float compactFactor = 10.0f;
+    private float economicFactor = 10.0f;
+    private float militaryFactor = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,20 +37,6 @@ public class AIManager : MonoBehaviour
     void Update()
     {
         enemyBuildings.RemoveAll(item => item == null);
-
-        spawnTimer += Time.deltaTime;
-
-        if (spawnTimer > spawnDelay)
-        {
-            spawnTimer = 0.0f;
-
-            SpawnEnemy();
-
-            groupSize *= spawnMultiplier;
-
-            if (spawnDelay > 10.0f)
-            spawnDelay -= 5.0f;
-        }
     }
 
     private void SpawnEnemy()
@@ -58,4 +56,95 @@ public class AIManager : MonoBehaviour
     {
         return enemyBuildings;
     }
+
+    private void ConstructNewBuilding(GameObject _buildingPrefab)
+    {
+        GameObject building = _buildingPrefab;
+        
+        if (!_buildingPrefab)
+        {
+            building = barracks;
+        }
+
+        float distanceAllowed = 0.0f;
+        BoxCollider box = building.GetComponent<BoxCollider>();
+
+        if (box.size.x > box.size.z)
+        {
+            distanceAllowed = box.size.x / 2;
+        }
+        else
+        {
+            distanceAllowed = box.size.z / 2;
+        }
+
+        Vector3 newPosition = enemyBuildings[0].transform.position;
+
+        float expansionDistance = 10.0f;
+
+        NavMeshHit nHit = new NavMeshHit();
+
+        int counter = 0;
+
+        do
+        {
+            newPosition = enemyBuildings[0].transform.position;
+
+            newPosition.x += Random.Range(-expansionDistance, 0);
+            newPosition.z += Random.Range(-expansionDistance, 0);
+
+            NavMesh.FindClosestEdge(newPosition, out nHit, NavMesh.AllAreas);
+
+            expansionDistance *= 1.1f;
+
+            counter++;
+
+            if (counter > 100)
+                break;
+
+        } while (Vector3.Distance(newPosition, enemyBuildings[0].transform.position) < 10 || nHit.distance < distanceAllowed || nHit.distance == Mathf.Infinity);//(nHit.distance < 10 || nHit.distance > 1000));
+
+        if (counter < 100)
+        {
+            GameObject newBuilding = Instantiate(building, newPosition, Quaternion.Euler(new Vector3(-90, 0, Random.Range(0, 359))));
+
+            Building build = newBuilding.GetComponent<Building>();
+            build.enabled = true;
+            build.ActivateObject();
+            newBuilding.tag = "EnemyBuilding";
+
+            enemyBuildings.Add(newBuilding);
+        }
+    }
+
+#if UNITY_EDITOR
+    [UnityEditor.CanEditMultipleObjects]
+    [UnityEditor.CustomEditor(typeof(AIManager))]
+    public class MachineEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            DrawDefaultInspector();
+
+            AIManager myScript = (AIManager)target;
+
+            if (GUILayout.Button("Spawn enemies"))
+            {
+                if (Application.isPlaying)
+                {
+                    myScript.SpawnEnemy();
+                }
+            }
+
+            if (GUILayout.Button("Spawn building"))
+            {
+                if (Application.isPlaying)
+                {
+                    myScript.ConstructNewBuilding(null);
+                }
+            }
+        }
+    }
+#endif
 }
